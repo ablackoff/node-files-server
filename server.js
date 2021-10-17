f928a77751cc1f0ab43928667054c2f539e9a78b
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2021-10-17 18:33:39 +0500

    Added eslint Fixed upload flow if upload dir is not exist Added editor config Added prettier config Changed all functions to arrow functions

--- server.js
+++ server.js
@@ -1,58 +1,68 @@
-import express from 'express';
-import multer  from 'multer';
-import path from 'path';
+import express from 'express'
+import multer from 'multer'
+import path from 'path'
+import fs from 'fs'
 
-const __dirname = path.resolve();
-const app = express();
+const __dirname = path.resolve()
+
+const uploadPath = path.join(__dirname, '/uploads/')
+
+const app = express()
 
 function mimeTypesFilter(mimeTypes) {
-  return function(req, file, cb) {
+  return (req, file, cb) => {
     if (mimeTypes.includes(file.mimetype)) {
-      cb(null, true);
+      cb(null, true)
     } else {
-      cb(new Error('File mime type error'));
+      cb(new Error('File mime type error'))
     }
-  };
+  }
 }
 
-const storage =   multer.diskStorage({
-	destination: function (req, file, callback) {
-		callback(null, './uploads');
-	},
-	filename: function (req, file, callback) {
-		callback(null, file.fieldname);
-	}
-});
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    fs.mkdir(uploadPath, (err) => {
+      if (err) {
+        throw new Error('You need to create the upload dir: "uploads" in the root folder')
+      }
 
-const mimeTypes = ['audio/mp3', 'audio/acc', 'audio/mpeg', 'audio/wav'];
+      callback(null, uploadPath)
+    })
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname)
+  },
+})
+
+const mimeTypes = ['audio/mp3', 'audio/acc', 'audio/mpeg', 'audio/wav']
 
 const upload = multer({
-	storage : storage,
-  fileFilter: mimeTypesFilter(mimeTypes)
-}).any();
+  storage: storage,
+  fileFilter: mimeTypesFilter(mimeTypes),
+}).any()
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
-app.get('/',function(req, res) {
-	res.sendFile(__dirname + "/index.html");
-});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
-app.post('/api/music',function(req, res) {
-	upload(req,res,function(err) {
-		if(err) {
-			return res.end(`Error uploading file.`);
-		}
-		res.end("File is uploaded");
-	});
-});
+app.post('/api/music', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.error(err)
+      return res.end(`Error uploading file.`)
+    }
 
-app.listen(9000,function() {
-	console.log(
-		"Working on port 9000\n" +
-		"Click here to open the link: http://localhost:9000"
-	);
-});
+    res.end('File is uploaded')
+  })
+})
+
+app.listen(9000, () => {
+  console.info('> App is started on port 9000')
+  console.info('> Click here to open the link: http://localhost:9000')
+})
